580adfe1c2b8734ded060920f4dea37c824998fb
Elvis Douglas Janegitz <edjelvis@gmail.com>
2019-04-02 22:41:32 -0300

    Create dedicated funcions for each dispatch

--- index.js
+++ index.js
@@ -1,71 +1,145 @@
-function createStore() {
+// Library Code
+function createStore (reducer) {
+  // The store should have four parts
+  // 1. The state
+  // 2. Get the state.
+  // 3. Listen to changes on the state.
+  // 4. Update the state
+
   let state
   let listeners = []
 
   const getState = () => state
 
-  // Função de monitoramento que é acionada depois da alteração do estado
   const subscribe = (listener) => {
     listeners.push(listener)
     return () => {
-      listeners = listeners.filter(l => l !== listener)
+      listeners = listeners.filter((l) => l !== listener)
     }
   }
 
-  // Função chamada com uma action que dispara a modificação
   const dispatch = (action) => {
-    state = todos(state, action)
-    listeners.forEach(listener => listener())
+    state = reducer(state, action)
+    listeners.forEach((listener) => listener())
   }
 
   return {
     getState,
     subscribe,
-    dispatch
+    dispatch,
   }
 }
 
 // App Code
-// Função reducer que modifica o estado
-function todos(state = [], action) {
-  if (action.type === 'ADD_TODO') {
-    return state.concat([action.todo])
-  }
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
 
-  return state
+function addTodoAction (todo) {
+  return {
+    type: ADD_TODO,
+    todo,
+  }
 }
 
-const store = createStore(todos)
-
-store.subscribe(() => console.log('O novo state é', store.getState()))
-
-// 1º dispacth exemplo
-store.dispatch({
-  type: 'ADD_TODO',
-  todo: {
-    id: 0,
-    name: 'Learn Redux',
-    complete: false
+function removeTodoAction (id) {
+  return {
+    type: REMOVE_TODO,
+    id,
   }
+}
+
+function toggleTodoAction (id) {
+  return {
+    type: TOGGLE_TODO,
+    id,
+  }
+}
+
+function addGoalAction (goal) {
+  return {
+    type: ADD_GOAL,
+    goal,
+  }
+}
+
+function removeGoalAction (id) {
+  return {
+    type: REMOVE_GOAL,
+    id,
+  }
+}
+
+function todos (state = [], action) {
+  switch(action.type) {
+    case ADD_TODO :
+      return state.concat([action.todo])
+    case REMOVE_TODO :
+      return state.filter((todo) => todo.id !== action.id)
+    case TOGGLE_TODO :
+      return state.map((todo) => todo.id !== action.id ? todo :
+        Object.assign({}, todo, { complete: !todo.complete }))
+    default :
+      return state
+  }
+}
+
+function goals (state = [], action) {
+  switch(action.type) {
+    case ADD_GOAL :
+      return state.concat([action.goal])
+    case REMOVE_GOAL :
+      return state.filter((goal) => goal.id !== action.id)
+    default :
+      return state
+  }
+}
+
+function app (state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action),
+  }
+}
+
+const store = createStore(app)
+
+store.subscribe(() => {
+  console.log('The new state is: ', store.getState())
 })
 
-// 2º dispacth exemplo
-store.dispatch({
-  type: 'ADD_TODO',
-  todo: {
-    id: 1,
-    name: 'Read book',
-    complete: false
-  }
-})
+store.dispatch(addTodoAction({
+  id: 0,
+  name: 'Walk the dog',
+  complete: false,
+}))
 
-// 3º dispacth exemplo
-store.dispatch({
-  type: 'ADD_TODO',
-  todo: {
-    id: 2,
-    name: 'Play game',
-    complete: true
-  }
-})
-// unsubscribe()
+store.dispatch(addTodoAction({
+  id: 1,
+  name: 'Wash the car',
+  complete: false,
+}))
+
+store.dispatch(addTodoAction({
+  id: 2,
+  name: 'Go to the gym',
+  complete: true,
+}))
+
+store.dispatch(removeTodoAction(1))
+
+store.dispatch(toggleTodoAction(0))
+
+store.dispatch(addGoalAction({
+  id: 0,
+  name: 'Learn Redux'
+}))
+
+store.dispatch(addGoalAction({
+  id: 1,
+  name: 'Lose 20 pounds'
+}))
+
+store.dispatch(removeGoalAction(0))
